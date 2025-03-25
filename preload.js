8ab7ef215dd4b3745ec8d327094bb1841ac36b2f
PHDinbeingLAZY <shanmukharap@gmail.com>
2025-03-25 21:24:01 +0400

    installing packages

--- preload.js
+++ preload.js
@@ -1,10 +1,9 @@
+// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
-
-// ... (previous code in preload.js) ...
 
 contextBridge.exposeInMainWorld('electronAPI', {
   getPythonVersion: () => ipcRenderer.invoke('get-python-version'),
   createVenv: (venvName, pythonPath) => ipcRenderer.invoke('create-venv', venvName, pythonPath),
   listPackages: (venvPath) => ipcRenderer.invoke('list-packages', venvPath),
-  // Add more API functions as needed
+  installPackage: (venvPath, packageName) => ipcRenderer.invoke('install-package', venvPath, packageName),
 });
